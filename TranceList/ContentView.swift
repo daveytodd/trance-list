b8bd0b2041cc6385febde03082b32a08998582bd
David Todd <daveytodd@gmail.com>
2026-07-29 08:51:27 +1000

    Initial Commit

---
+++ TranceList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranceList
+//
+//  Created by David Todd on 29/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranceList/TranceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranceListApp.swift
+//  TranceList
+//
+//  Created by David Todd on 29/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct TranceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
